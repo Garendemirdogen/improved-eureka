23277a29b73d8874b1289e842238552f16a072ec
Garen <garendemirdogen1@gmail.com>
2022-08-13 19:29:04 -0400

    added manager inputs

--- index.js
+++ index.js
@@ -12,3 +12,37 @@ const inquirer = require("inquirer");
 
 // team array
 const teamArray = [];
+
+// start of manager prompts
+const addManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Who is the manager of this team?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the manager's ID.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the manager's email.",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the manager's office number",
+      },
+    ])
+    .then((managerInput) => {
+      const { name, id, email, officeNumber } = managerInput;
+      const manager = new Manager(name, id, email, officeNumber);
+
+      teamArray.push(manager);
+      console.log(manager);
+    });
+};
