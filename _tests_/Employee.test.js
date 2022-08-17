8982bad17a0cf6b6b6fbf3f044accd2731fda1f8
Garen <garendemirdogen1@gmail.com>
2022-08-16 21:54:02 -0400

    created employee object

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,2 +1,10 @@
 // using Employee constructor
 const Employee = require("../lib/Employee");
+// creates employee object
+test("creates an employee object", () => {
+  const employee = new Employee("Garen", 27, "Garendemirdogen1@gmail.com");
+
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+});
