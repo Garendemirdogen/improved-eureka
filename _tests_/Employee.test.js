df20722d04b5c745bdc7946d212da0edcdab54b2
Garen <garendemirdogen1@gmail.com>
2022-08-16 22:23:21 -0400

    added tests for employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -8,3 +8,32 @@ test("creates an employee object", () => {
   expect(employee.id).toEqual(expect.any(Number));
   expect(employee.email).toEqual(expect.any(String));
 });
+// gets id from getId()
+test("gets employee name", () => {
+  const employee = new Employee("Garen", 27, "Garendemirdogen1@gmail.com");
+
+  expect(employee.getName()).toEqual(expect.any(String));
+});
+
+// gets id from getId()
+test("gets employee ID", () => {
+  const employee = new Employee("Garen", 27, "Garendemirdogen1@gmail.com");
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+// gets emails from getEmail()
+test("gets employee email", () => {
+  const employee = new Employee("Garen", 27, "Garendemirdogen1@gmail.com");
+
+  expect(employee.getEmail()).toEqual(
+    expect.stringContaining(employee.email.toString())
+  );
+});
+
+// gets role from getRole()
+test("gets role of employee", () => {
+  const employee = new Employee("Garen", 27, "Garendemirdogen1@gmail.com");
+
+  expect(employee.getRole()).toEqual("Employee");
+});
