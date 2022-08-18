08b5a35bcc40a1c2e5b9f5a4623d1b782257f8dd
Garen <garendemirdogen1@gmail.com>
2022-08-17 21:44:39 -0400

    updated intern test

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,14 +2,14 @@
 const Intern = require("../lib/Intern");
 // creating intern object
 test("creates an Intern object", () => {
-  const intern = new Intern("Mac", 10, "Mac.Jones10@gmail", "UA");
+  const intern = new Intern("Mac Jones", 10, "Macjones10@gmail", "UA");
 
   expect(intern.school).toEqual(expect.any(String));
 });
 
 // gets school from getSchool()
 test("gets employee school", () => {
-  const intern = new Intern("Mac", 10, "Mac.Jones10@gmail", "UA");
+  const intern = new Intern("Mac Jones", 10, "Macjones10@gmail", "UA");
 
   expect(intern.getSchool()).toEqual(
     expect.stringContaining(intern.school.toString())
@@ -18,7 +18,7 @@ test("gets employee school", () => {
 
 // gets role from getRole()
 test("gets role of employee", () => {
-  const intern = new Intern("Mac", 10, "Mac.Jones10@gmail.com", "UA");
+  const intern = new Intern("Mac Jones", 10, "Macjones10@gmail.com", "UA");
 
   expect(intern.getRole()).toEqual("Intern");
 });
